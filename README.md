648950c174014c586aa813c1426c9391d538463b
Smit-1911 <164208010+Smit-1911@users.noreply.github.com>
2024-08-07 19:10:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# HACKATHON
+# ServiceSphere
+Hackathon
---
+++ script.js
@@ -0,0 +1,17 @@
+const carousel = document.querySelector(".carousel")
+  
+
+let isDragging = false, startX, scrollLeft;
+
+
+carousel.addEventListener("mousedown", e => {
+  isDragging = true;
+  startX = e.pageX;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener("mousemove", e => {
+  if (isDragging) carousel.scrollLeft = scrollLeft - (e.pageX - startX) * 2;
+});
+
+carousel.addEventListener("mouseup", () => isDragging = false);
